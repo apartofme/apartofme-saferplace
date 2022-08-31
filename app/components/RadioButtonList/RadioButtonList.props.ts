04402b8eb6d7dbaecdd4744408f791ce042547e1
Vladyslav Riabko <vladislav.rya0@gmail.com>
2022-08-31 19:08:05 +0300

    refactor(language-selection-screen): PR changes

--- app/components/RadioButtonList/RadioButtonList.props.ts
+++ app/components/RadioButtonList/RadioButtonList.props.ts
@@ -6,5 +6,5 @@ import {
 export interface IRadioButtonListProps {
   data: IRadioButtonListItem[];
   type: RadioButtonListType;
-  setSelected: (item: string[]) => void;
+  setSelected: (items: string[]) => void;
 }
