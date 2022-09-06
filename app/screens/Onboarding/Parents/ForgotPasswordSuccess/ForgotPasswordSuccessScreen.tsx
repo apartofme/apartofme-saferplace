ee2aa40f68f10c4717899190e9055d033a8d1698
Andrii Khlivnyi <akhlivnoy@gmail.com>
2022-09-06 13:44:07 +0300

    refactor(forgot-password-success): replace to reusable components

--- app/screens/Onboarding/Parents/ForgotPasswordSuccess/ForgotPasswordSuccessScreen.tsx
+++ app/screens/Onboarding/Parents/ForgotPasswordSuccess/ForgotPasswordSuccessScreen.tsx
@@ -1,8 +1,10 @@
+import _ from 'lodash';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { SafeAreaView } from 'react-native';
 
-import { ExtendedButton, ExtendedText } from '../../../../components';
+import { BottomButtonView, ExtendedText } from '../../../../components';
+import { generalStyles } from '../../../../utils/styles';
 import { IForgotPasswordSuccessScreenProps } from './ForgotPasswordSuccessScreen.props';
 
 export const ForgotPasswordSuccessScreen: React.FC<IForgotPasswordSuccessScreenProps> =
@@ -10,14 +12,18 @@ export const ForgotPasswordSuccessScreen: React.FC<IForgotPasswordSuccessScreenP
     const { t } = useTranslation();
 
     return (
-      <SafeAreaView>
-        <ExtendedText>
-          {t('screens.onboarding.forgot_password.success.title')}
-        </ExtendedText>
-        <ExtendedText>
-          {t('screens.onboarding.forgot_password.success.subtitle')}
-        </ExtendedText>
-        <ExtendedButton title={t('buttons.open_email')} />
+      <SafeAreaView style={generalStyles.whFlex}>
+        {/* // TODO: change to correct function */}
+        <BottomButtonView
+          buttonTitle={t('buttons.open_email')}
+          onSubmit={_.noop}>
+          <ExtendedText>
+            {t('screens.onboarding.forgot_password.success.title')}
+          </ExtendedText>
+          <ExtendedText>
+            {t('screens.onboarding.forgot_password.success.subtitle')}
+          </ExtendedText>
+        </BottomButtonView>
       </SafeAreaView>
     );
   };
