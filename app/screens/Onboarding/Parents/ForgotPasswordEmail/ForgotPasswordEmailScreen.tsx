ee9aec5805cdf0027da7cd5fec7a73ec5786d30f
Andrii Khlivnyi <akhlivnoy@gmail.com>
2022-09-06 13:39:24 +0300

    refactor(forgot-password-email): replace to reusable components

--- app/screens/Onboarding/Parents/ForgotPasswordEmail/ForgotPasswordEmailScreen.tsx
+++ app/screens/Onboarding/Parents/ForgotPasswordEmail/ForgotPasswordEmailScreen.tsx
@@ -1,44 +1,45 @@
+import _ from 'lodash';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import {
-  KeyboardAvoidingView,
-  SafeAreaView,
-  TouchableOpacity,
-} from 'react-native';
+import { KeyboardAvoidingView, SafeAreaView } from 'react-native';
 
+import { IMAGES } from '../../../../assets';
 import {
-  ExtendedButton,
+  BottomButtonView,
   ExtendedText,
   ExtendedTextInput,
   ExtendedTextInputType,
+  MainHeader,
 } from '../../../../components';
+import { generalStyles } from '../../../../utils/styles';
 import { IForgotPasswordEmailScreenProps } from './ForgotPasswordEmailScreen.props';
-import { styles } from './ForgotPasswordEmailScreen.styles';
 
 export const ForgotPasswordEmailScreen: React.FC<IForgotPasswordEmailScreenProps> =
   ({ navigation }) => {
     const { t } = useTranslation();
 
     return (
-      <SafeAreaView>
-        <TouchableOpacity
-          onPress={navigation.goBack}
-          style={styles.goBackButton}>
-          {/* // TODO: add the image */}
-        </TouchableOpacity>
-
-        <KeyboardAvoidingView behavior="padding">
-          <ExtendedText>
-            {t('screens.onboarding.forgot_password.email.title')}
-          </ExtendedText>
-          <ExtendedText>
-            {t('screens.onboarding.forgot_password.email.subtitle')}
-          </ExtendedText>
-          <ExtendedTextInput
-            type={ExtendedTextInputType.Email}
-            placeholder={t('placeholders.enter_email')}
-          />
-          <ExtendedButton title={t('buttons.reset_password')} />
+      <SafeAreaView style={generalStyles.whFlex}>
+        <MainHeader
+          leftIcon={IMAGES.WHITE_BACK_ARROW}
+          onLeftIconPress={navigation.goBack}
+        />
+        <KeyboardAvoidingView style={generalStyles.flex} behavior="padding">
+          {/* // TODO: change to correct function */}
+          <BottomButtonView
+            buttonTitle={t('buttons.reset_password')}
+            onSubmit={_.noop}>
+            <ExtendedText>
+              {t('screens.onboarding.forgot_password.email.title')}
+            </ExtendedText>
+            <ExtendedText>
+              {t('screens.onboarding.forgot_password.email.subtitle')}
+            </ExtendedText>
+            <ExtendedTextInput
+              type={ExtendedTextInputType.Email}
+              placeholder={t('placeholders.enter_email')}
+            />
+          </BottomButtonView>
         </KeyboardAvoidingView>
       </SafeAreaView>
     );
