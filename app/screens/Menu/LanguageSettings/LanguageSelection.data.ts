dcd333ea899fcb0bbd62e2c3967638b7a81d636e
Vladyslav Riabko <vladislav.rya0@gmail.com>
2022-09-06 14:43:19 +0300

    feat(language-settings-screen): language change logic

---
+++ app/screens/Menu/LanguageSettings/LanguageSelection.data.ts
@@ -0,0 +1,7 @@
+import { IRadioButtonListItem } from '../../../../components';
+
+export const LANGUAGES: ReadonlyArray<IRadioButtonListItem> = [
+  { id: 'en', title: 'languages.english' },
+  { id: 'uk', title: 'languages.ukrainian' },
+  { id: 'rus', title: 'languages.russian' },
+];
--- app/screens/Menu/LanguageSettings/LanguageSettings.tsx
+++ app/screens/Menu/LanguageSettings/LanguageSettings.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { SafeAreaView, View } from 'react-native';
 
 import { IMAGES } from '../../../assets';
-import { ExtendedButton, ExtendedText, MainHeader } from '../../../components';
+import {
+  ExtendedButton,
+  ExtendedText,
+  MainHeader,
+  RadioButtonList,
+  RadioButtonListType,
+} from '../../../components';
+import { useAppDispatch, useAppSelector } from '../../../hooks';
+import { settingsSlice } from '../../../redux/slices';
+import i18n from '../../../services/localization';
+import { LANGUAGES } from './LanguageSelection.data';
 import { ILanguageSettingsScreenProps } from './LanguageSettings.props';
 import { styles } from './LanguageSettings.styles';
 
@@ -12,24 +22,46 @@ export const LanguageSettingsScreen: React.FC<ILanguageSettingsScreenProps> = ({
 }) => {
   const { t } = useTranslation();
 
+  const disptach = useAppDispatch();
+
+  const [selectedLanguage, setSelectedLanguage] = useState<string[]>([
+    useAppSelector(state => state.settings.language),
+  ]);
+
+  const onSubmit = useCallback(() => {
+    i18n.changeLanguage(selectedLanguage[0]);
+    disptach(
+      settingsSlice.actions.setLanguage({ language: selectedLanguage[0] }),
+    );
+  }, [disptach, selectedLanguage]);
+
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <MainHeader
-          // TODO: add right icon
-          leftIcon={IMAGES.WHITE_BACK_ARROW}
-        />
+      <MainHeader
+        // TODO: add right icon
+        leftIcon={IMAGES.WHITE_BACK_ARROW}
+        onLeftIconPress={navigation.goBack}
+      />
+      <View style={styles.contentContainer}>
         <ExtendedText style={styles.title}>
           {t('screens.menu.language_settings.title')}
         </ExtendedText>
         <ExtendedText style={styles.subtitle}>
           {t('screens.menu.language_settings.subtitle')}
         </ExtendedText>
+        <View style={styles.listContainer}>
+          <RadioButtonList
+            data={LANGUAGES}
+            type={RadioButtonListType.Single}
+            setSelected={setSelectedLanguage}
+          />
+        </View>
       </View>
 
-      <View>
-        <ExtendedButton title={t('buttons.save')} />
+      <View style={styles.buttonContainer}>
+        <ExtendedButton title={t('buttons.save')} onPress={onSubmit} />
         <ExtendedButton
+          style={styles.cancelButton}
           title={t('buttons.cancel')}
           onPress={navigation.goBack}
         />
