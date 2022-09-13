6c11cb64ad140d34f3234e59ff93b3f53a4b4819
Andrii Khlivnyi <akhlivnoy@gmail.com>
2022-09-13 14:21:06 +0300

    refactor(onboarding-positioning): ForgotPasswordSuccessScreen positioning

--- app/screens/Onboarding/Parents/ForgotPasswordSuccess/ForgotPasswordSuccessScreen.tsx
+++ app/screens/Onboarding/Parents/ForgotPasswordSuccess/ForgotPasswordSuccessScreen.tsx
@@ -29,11 +29,11 @@ export const ForgotPasswordSuccessScreen: React.FC<IForgotPasswordSuccessScreenP
           onSubmit={_.noop}
           style={styles.container}>
           {/* // TODO: add the image */}
-          <ExtendedText style={styles.title}>
+          <ExtendedText preset="large-title" style={styles.title}>
             {t('screens.onboarding.forgot_password.success.title')}
           </ExtendedText>
 
-          <ExtendedText style={styles.subtitle}>
+          <ExtendedText preset="secondary-text" style={styles.subtitle}>
             {t('screens.onboarding.forgot_password.success.subtitle')}
           </ExtendedText>
         </BottomButtonView>
