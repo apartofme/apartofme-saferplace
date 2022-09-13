c12b66593097248b9f47337691cdc23fbfaac722
Andrii Khlivnyi <akhlivnoy@gmail.com>
2022-09-13 13:41:31 +0300

    refactor(onboarding-positioning): LoadingScreen positioning

--- app/services/localization/languages/en.ts
+++ app/services/localization/languages/en.ts
@@ -130,7 +130,7 @@ export const en = {
               'You can now introduce your child to A Safer Place. You can do this whenever is best for both of you.',
           },
           second: {
-            title: 'Funny random loading thing...',
+            title: 'Loading Forest...',
             subtitle:
               'We suggest telling your child that you have found a magical game to play together, a game designed to help us get through difficult times.',
           },
