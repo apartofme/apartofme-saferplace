d4920c81ad7664777288b8b34b317b9d6e0c88d8
Andrii Khlivnyi <akhlivnoy@gmail.com>
2022-09-15 13:17:10 +0300

    feat(select-player-screen): reusable SelectPlayerScreen UX & positioning

--- app/components/RadioButton/RadioButton.tsx
+++ app/components/RadioButton/RadioButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { Image, TouchableOpacity } from 'react-native';
 
 import { ExtendedText } from '../ExtendedText';
 import { IRadioButtonProps } from './RadioButton.types';
@@ -9,14 +9,17 @@ export const RadioButton: React.FC<IRadioButtonProps> = ({
   title,
   isActive = false,
   onPress,
+  style,
+  imageSource,
 }) => {
   return (
     <TouchableOpacity
-      style={[styles.container, isActive && styles.activeBackground]}
+      style={[styles.container, isActive && styles.activeBackground, style]}
       onPress={onPress}>
       <ExtendedText style={[styles.title, isActive && styles.activeTitle]}>
         {title}
       </ExtendedText>
+      {imageSource && <Image source={imageSource} />}
     </TouchableOpacity>
   );
 };
