1a5503caef7d60ecbf2419b0fe1cbd7ce2b27357
Vladyslav Riabko <vladislav.rya0@gmail.com>
2022-09-16 17:41:11 +0300

    fix(text-input-keyboard): Feedback & ChangePassword inputs

--- app/screens/Menu/ChangePassword/ChangePassword.tsx
+++ app/screens/Menu/ChangePassword/ChangePassword.tsx
@@ -6,6 +6,7 @@ import { SafeAreaView, View } from 'react-native';
 import { IMAGES } from '../../../assets';
 import {
   BottomButtonView,
+  ExtendedKeyboardAvoidingView,
   ExtendedText,
   ExtendedTextInput,
   MainHeader,
@@ -21,34 +22,38 @@ export const ChangePasswordScreen: React.FC<IChangePasswordScreenProps> = ({
 
   return (
     <SafeAreaView style={generalStyles.flex}>
-      <BottomButtonView
-        buttonTitle={t('buttons.reset_password')}
-        onSubmit={_.noop}>
-        <MainHeader
-          leftIcon={IMAGES.WHITE_BACK_ARROW}
-          onLeftIconPress={navigation.goBack}
-        />
-        <View style={styles.container}>
-          <ExtendedText style={styles.title}>Change your password</ExtendedText>
-          <ExtendedText>
-            {t('screens.menu.change_password.description')}
-          </ExtendedText>
-          <View style={styles.inputContainer}>
-            <ExtendedTextInput
-              style={styles.input}
-              placeholder={t('placeholders.enter_current_password')}
-            />
-            <ExtendedTextInput
-              style={styles.input}
-              placeholder={t('placeholders.enter_new_password')}
-            />
-            <ExtendedTextInput
-              style={styles.input}
-              placeholder={t('placeholders.confirm_new_password')}
-            />
+      <ExtendedKeyboardAvoidingView>
+        <BottomButtonView
+          buttonTitle={t('buttons.reset_password')}
+          onSubmit={_.noop}>
+          <MainHeader
+            leftIcon={IMAGES.WHITE_BACK_ARROW}
+            onLeftIconPress={navigation.goBack}
+          />
+          <View style={styles.container}>
+            <ExtendedText style={styles.title}>
+              Change your password
+            </ExtendedText>
+            <ExtendedText>
+              {t('screens.menu.change_password.description')}
+            </ExtendedText>
+            <View style={styles.inputContainer}>
+              <ExtendedTextInput
+                style={styles.input}
+                placeholder={t('placeholders.enter_current_password')}
+              />
+              <ExtendedTextInput
+                style={styles.input}
+                placeholder={t('placeholders.enter_new_password')}
+              />
+              <ExtendedTextInput
+                style={styles.input}
+                placeholder={t('placeholders.confirm_new_password')}
+              />
+            </View>
           </View>
-        </View>
-      </BottomButtonView>
+        </BottomButtonView>
+      </ExtendedKeyboardAvoidingView>
     </SafeAreaView>
   );
 };
