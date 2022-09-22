ebbf1cafb5847f4044af41ad4e62e5820a34c1ab
Andrii Khlivnyi <akhlivnoy@gmail.com>
2022-09-22 13:41:00 +0300

    refactor(radio-button-screen): PR changes

--- app/screens/Reusable/RadioButton/RadioButton.data.ts
+++ app/screens/Reusable/RadioButton/RadioButton.data.ts
@@ -1,6 +1,6 @@
 import { IRadioButtonListItem } from '../../../components';
 
-export const RADIO_BUTTON_LIST: IRadioButtonListItem[] = [
+export const RADIO_BUTTON_LIST: ReadonlyArray<IRadioButtonListItem> = [
   {
     id: 'yes',
     title: 'Yes',
