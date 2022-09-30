b7fe6ce64a8d4772ec228aebfca45cd16140937f
Vladislav <vladislav.rya0@gmail.com>
2022-09-30 03:10:25 +0300

    refactor(recognition-acknowledgement-screen): navigation

--- app/navigation/stacks/recognitionStackNavigator.tsx
+++ app/navigation/stacks/recognitionStackNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from 'react-native-screens/native-stack';
 
 import {
+  ElixirCarouselScreen,
   IRecognitionAcknowledgementRoute,
   RecognitionAcknowledgementScreen,
 } from '../../screens';
@@ -9,6 +10,7 @@ import { DEFAULT_STACK_OPTIONS } from '../options';
 
 export type RecognitionStackParams = {
   RecognitionAcknowledgement: { data: IRecognitionAcknowledgementRoute };
+  ElixirCarousel: undefined;
 };
 
 const Stack = createNativeStackNavigator<RecognitionStackParams>();
@@ -19,5 +21,6 @@ export const RecognitionStackNavigator = () => (
       name="RecognitionAcknowledgement"
       component={RecognitionAcknowledgementScreen}
     />
+    <Stack.Screen name="ElixirCarousel" component={ElixirCarouselScreen} />
   </Stack.Navigator>
 );
--- app/screens/Recognition/index.ts
+++ app/screens/Recognition/index.ts
@@ -1,1 +1,2 @@
 export * from './RecognitionAcknowledgement';
+export * from './ElixirCarousel';
