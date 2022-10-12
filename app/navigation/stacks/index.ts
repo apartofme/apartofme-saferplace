daed0dac30d79728a97a46d1d27ff3879865a62f
Vladislav <vladislav.rya0@gmail.com>
2022-10-12 22:33:55 +0300

    feat(parent-grounding-stack): ParentGroundingStackNavigator

--- app/navigation/stacks/index.ts
+++ app/navigation/stacks/index.ts
@@ -2,6 +2,7 @@ export * from './gardenStackNavigator';
 export * from './jointOnboardingStackNavigator';
 export * from './menuStackNavigator';
 export * from './mixingElixirStackNavigator';
+export * from './parentGroundingStackNavigator';
 export * from './parentsOnboardingStackNavigator';
 export * from './questStackNavigator';
 export * from './recognitionStackNavigator';
