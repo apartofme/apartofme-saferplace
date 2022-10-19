ad3e842a55d7d58eb8ae9f9c469ad011717c6b9d
Anna <hnybidenkowork@gmail.com>
2022-10-19 16:19:15 +0100

    fix(develop)

--- app/assets/images/index.ts
+++ app/assets/images/index.ts
@@ -8,12 +8,10 @@ export const IMAGES = {
   BOOK_ON_LEAVES: require('./bookOnLeaves.png'),
   BROWN_TRIANGLE: require('./brownTriangle.png'),
   CHECK_MARK: require('./checkMark.png'),
-  CLOSED_BOOK: require('./closedBook.png'),
   COMPASSIONATE_GUIDE: require('./compassionateGuide.png'),
   HAPPY_TREE_ON_GRASS: require('./happyTreeOnGrass.png'),
   MUSHROOM_MAN_IN_TREES: require('./mushroomManInTrees.png'),
   NADIYA: require('./nadiya.png'),
-  OPEN_BOOK: require('./openBook.png'),
   TREE: require('./tree.png'),
   WHITE_BACK_ARROW: require('./whiteBackArrow.png'),
 };
