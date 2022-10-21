8c276ce486b54f33086ff743d4d2c83e589d3b3a
Vladislav <vladislav.rya0@gmail.com>
2022-10-22 02:46:07 +0300

    feat(notification-settings-screen-ui): NotificationSettingsScreen

--- app/screens/Menu/NotificationSettings/NotificationSettings.tsx
+++ app/screens/Menu/NotificationSettings/NotificationSettings.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ImageBackground, SafeAreaView, View } from 'react-native';
 
-import { ExtendedButton, ExtendedText, MainHeader } from '../../../components';
+import { ExtendedText, MainHeader } from '../../../components';
 import { useAppDispatch, useAppSelector } from '../../../hooks';
 import { settingsSlice } from '../../../redux/slices';
 import { generalStyles } from '../../../utils/styles';
@@ -52,18 +52,24 @@ export const NotificationSettingsScreen: React.FC<INotificationSettingsScreenPro
               {t('screens.menu.notification_settings.title')}
             </ExtendedText>
 
-            <MenuSwitchRow
-              title={t(
-                'screens.menu.notification_settings.is_allow_notifications',
-              )}
-              isEnabled={isNotificationsEnabled}
-              setIsEnabled={setNotificationEnabled}
-            />
-            <ExtendedButton
-              title="To device settings"
-              style={styles.button}
-              onPress={openDeviceNotificationSettings}
-            />
+            <View style={styles.menuSwitchContainer}>
+              <MenuSwitchRow
+                title={t(
+                  'screens.menu.notification_settings.is_allow_notifications',
+                )}
+                isEnabled={isNotificationsEnabled}
+                setIsEnabled={setNotificationEnabled}
+              />
+              <ExtendedText style={styles.subtitle} preset="secondary-text">
+                {t('screens.menu.notification_settings.description')}
+              </ExtendedText>
+              <ExtendedText
+                preset="secondary-text"
+                style={[generalStyles.boldText, generalStyles.primaryOrange]}
+                onPress={openDeviceNotificationSettings}>
+                {t('labels.system_settings')}
+              </ExtendedText>
+            </View>
           </View>
         </SafeAreaView>
       </ImageBackground>
