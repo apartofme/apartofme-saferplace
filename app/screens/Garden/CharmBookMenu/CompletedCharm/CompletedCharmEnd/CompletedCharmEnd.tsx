e5b471d253fe547587bde283fa2cfcfd30219c7e
Andrii Khlivnyi <akhlivnoy@gmail.com>
2022-10-23 20:18:17 +0300

    feat(charm-book-menu-ui): CompletedCharmEndScreen UI

--- app/screens/Garden/CharmBookMenu/CompletedCharm/CompletedCharmEnd/CompletedCharmEnd.tsx
+++ app/screens/Garden/CharmBookMenu/CompletedCharm/CompletedCharmEnd/CompletedCharmEnd.tsx
@@ -1,22 +1,32 @@
 import React, { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
-import { SafeAreaView } from 'react-native';
+import { ImageBackground, SafeAreaView } from 'react-native';
 
-import { ExtendedButton, ExtendedText } from '../../../../../components';
+import { BACKGROUND_IMAGES } from '../../../../../assets';
+import { SVG } from '../../../../../assets/svg';
+import {
+  BottomButtonView,
+  ExtendedText,
+  MainHeader,
+} from '../../../../../components';
 import { useParsedJSXTextNickname } from '../../../../../hooks';
 import { COLORS } from '../../../../../themes/colors';
 import { generalStyles } from '../../../../../utils/styles';
+import { styles } from './CompletedCharmEnd.styles';
 import { ICompletedCharmEndScreenProps } from './CompletedCharmEnd.types';
+
+const WhiteBackArrowIcon = SVG.WhiteBackArrowIcon;
+const CelebrationGuideIcon = SVG.CelebrationGuideIcon;
 
 export const CompletedCharmEndScreen: React.FC<ICompletedCharmEndScreenProps> =
   ({ navigation }) => {
     const { t } = useTranslation();
 
     const Title = useParsedJSXTextNickname({
-      text: t('screens.completed-charm-end.title'),
+      text: t('screens.completed_charm_end.title'),
       textHasNickname: true,
       preset: 'title',
-      // style: styles.title,
+      style: styles.title,
       variableStyle: { color: COLORS.PRIMARY_ORANGE },
     });
 
@@ -32,19 +42,26 @@ export const CompletedCharmEndScreen: React.FC<ICompletedCharmEndScreenProps> =
     }, [navigation]);
 
     return (
-      <SafeAreaView style={generalStyles.flex}>
-        {/* // TODO: uncoment when icon will be done */}
-        {/* <MainHeader
-          leftIcon={IMAGES.WHITE_BACK_ARROW}
-          onLeftIconPress={navigation.goBack}
-        /> */}
-        {/* // TODO: change to real image */}
-        {/* <Image source={IMAGES.LOGO} /> */}
-        <Title />
-        <ExtendedText>
-          {t('screens.completed-charm-end.description')}
-        </ExtendedText>
-        <ExtendedButton title="buttons.next" onPress={onSabmit} />
-      </SafeAreaView>
+      <ImageBackground
+        source={BACKGROUND_IMAGES.GARDEN_DARK}
+        style={generalStyles.flex}>
+        <SafeAreaView style={generalStyles.flex}>
+          <MainHeader
+            leftIcon={<WhiteBackArrowIcon />}
+            onLeftIconPress={navigation.goBack}
+          />
+          <BottomButtonView
+            buttonTitle={t('buttons.next')}
+            isArrow={true}
+            onSubmit={onSabmit}
+            style={styles.container}>
+            <CelebrationGuideIcon />
+            <Title />
+            <ExtendedText style={generalStyles.greyCenter}>
+              {t('screens.completed_charm_end.description')}
+            </ExtendedText>
+          </BottomButtonView>
+        </SafeAreaView>
+      </ImageBackground>
     );
   };
