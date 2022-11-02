6387eda741cd84fed6ce817a2044605cfaec5ef3
Andrii Khlivnyi <akhlivnoy@gmail.com>
2022-11-02 19:19:07 +0200

    refactor(validation)

--- app/screens/Menu/ChangePassword/ChangePassword.validation.tsx
+++ app/screens/Menu/ChangePassword/ChangePassword.validation.tsx
@@ -1,28 +1,11 @@
 import * as yup from 'yup';
 
-import { REGEXPS } from '../../../utils/regexps';
-
 export const ChangePasswordValidationSchema = yup.object().shape({
-  currentPassword: yup
-    .string()
-    .matches(REGEXPS.UPPERCASE, 'Password must include UPPERCASE')
-    .matches(REGEXPS.LOWERCASE, 'Password must include LOWERCASE')
-    .matches(REGEXPS.NUMBER, 'Password must include NUMBER')
-    .min(8, ({ min }) => `Password must somebe ${min} characters`)
-    .required('Please enter this fild'),
-  newPassword: yup
-    .string()
-    .matches(REGEXPS.UPPERCASE, 'Password must include UPPERCASE')
-    .matches(REGEXPS.LOWERCASE, 'Password must include LOWERCASE')
-    .matches(REGEXPS.NUMBER, 'Password must include NUMBER')
-    .min(8, ({ min }) => `Password must somebe ${min} characters`)
-    .required('Please enter this fild'),
+  currentPassword: yup.string().min(6).required(),
+  newPassword: yup.string().min(6).required(),
   confirmNewPassword: yup
     .string()
-    .matches(REGEXPS.UPPERCASE, 'Password must include UPPERCASE')
-    .matches(REGEXPS.LOWERCASE, 'Password must include LOWERCASE')
-    .matches(REGEXPS.NUMBER, 'Password must include NUMBER')
-    .min(8, ({ min }) => `Password must somebe ${min} characters`)
-    .oneOf([yup.ref('newPassword')], 'Both passwords need to be the same')
-    .required('Please enter this fild'),
+    .min(6)
+    .oneOf([yup.ref('newPassword')])
+    .required(),
 });
--- app/screens/Onboarding/Parents/SignIn/Login/Login.validation.ts
+++ app/screens/Onboarding/Parents/SignIn/Login/Login.validation.ts
@@ -1,17 +1,6 @@
 import * as yup from 'yup';
 
-import { REGEXPS } from '../../../../../utils/regexps';
-
 export const SignInValidationSchema = yup.object().shape({
-  email: yup
-    .string()
-    .email('Incorrect email')
-    .required('Please enter this fild'),
-  password: yup
-    .string()
-    .matches(REGEXPS.UPPERCASE, 'Password must include UPPERCASE')
-    .matches(REGEXPS.LOWERCASE, 'Password must include LOWERCASE')
-    .matches(REGEXPS.NUMBER, 'Password must include NUMBER')
-    .min(8, ({ min }) => `Password must contain at least ${min} characters`)
-    .required('Please enter this fild'),
+  email: yup.string().email().required(),
+  password: yup.string().min(6).required(),
 });
