f9617ba9ddefc0896031b58cae99d99923e9f3c4
Vladislav <vladislav.rya0@gmail.com>
2022-11-04 20:17:50 +0200

    feat(loading-animation): Meditative

--- app/screens/Reusable/Timers/CharmTimerButton/CharmTimerButton.styles.ts
+++ app/screens/Reusable/Timers/CharmTimerButton/CharmTimerButton.styles.ts
@@ -1,23 +1,31 @@
 import { StyleSheet, ViewStyle } from 'react-native';
 
-import { WINDOW_COEFFICIENT } from '../../../../constants/window';
+import { WINDOW_COEFFICIENT, WINDOW_WIDTH } from '../../../../constants/window';
 import { COLORS } from '../../../../themes/colors';
 import { hexTransparency } from '../../../../utils';
 
 interface ICharmTimerButtonScreenStyles {
   container: ViewStyle;
-  timer: ViewStyle;
   readLoudContainer: ViewStyle;
+  animation: ViewStyle;
+  headerContainer: ViewStyle;
 }
 
 export const styles = StyleSheet.create<ICharmTimerButtonScreenStyles>({
   container: {
     paddingTop: 16 * WINDOW_COEFFICIENT,
+    paddingBottom: 40 * WINDOW_COEFFICIENT,
     paddingHorizontal: 40,
     alignItems: 'center',
+    justifyContent: 'space-between',
   },
-  timer: {
-    paddingVertical: 86 * WINDOW_COEFFICIENT,
+  headerContainer: {
+    zIndex: 10,
+  },
+  animation: {
+    width: WINDOW_WIDTH,
+    position: 'absolute',
+    top: WINDOW_COEFFICIENT * 75 - 100,
   },
   readLoudContainer: {
     backgroundColor: hexTransparency(COLORS.PRIMARY_PURPLE, 70),
