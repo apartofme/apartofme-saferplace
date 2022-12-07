e04e40957a7b7f02ef7b74dada64a1682e5fcdbc
Anna <hnybidenkowork@gmail.com>
2022-12-07 20:34:14 +0000

    release 1.0.22

--- app/sagas/localization.ts
+++ app/sagas/localization.ts
@@ -1,4 +1,4 @@
-import { call, put, select } from 'redux-saga/effects';
+import { call, delay, put, select } from 'redux-saga/effects';
 
 import CONFIG from '../config/env';
 import { cacheSlice } from '../redux/slices';
@@ -10,6 +10,13 @@ import { getIsConnected } from '../utils';
 
 export function* initLocalizationSaga() {
   const locales = CONFIG.LANGUAGES;
+
+  // *** Fix for localization: the correct value of the selected language from the state does not have time to load
+  // intentionally
+  yield delay(10);
+  const settingsLanguage: string = yield select(
+    state => state.settings.settings.language,
+  );
 
   const isConnected: boolean = yield call(getIsConnected);
 
@@ -25,6 +32,11 @@ export function* initLocalizationSaga() {
             [language]: { translations },
           }),
         );
+
+        const currentLanguageTranslation: Record<string, string> = yield select(
+          state => state.cache.translations[settingsLanguage].translations,
+        );
+        yield call(setLocalizationBundle, language, currentLanguageTranslation);
       } else {
         const reduxTranslations: Record<string, string> = yield select(
           state => state.cache.translations[language].translations,
